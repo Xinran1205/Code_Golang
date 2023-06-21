acb70e577a486ea2454169b0dcdf8a55d112c815
Xinran1205 <id19037@bristol.ac.uk>
2023-06-21 12:12:12 +0800

    GoRoutine Code

--- GoRoutine/Entrance.go
+++ GoRoutine/Entrance.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func newTask() {
+	i := 0
+	for {
+		i++
+		fmt.Println("new goroutine: i = ", i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	//创建一个goRoutine，去执行newTask()函数
+	//这个就是理解为我以前学的c语言的fork，创建的是子进程
+	go newTask()
+	//所以我们如果在这就退出main函数，那么newTask()函数就不会执行了
+	//因为main理解为父进程，父进程退出了，子进程也就退出了
+
+	//但是我们可以让main函数休眠一段时间，让newTask()函数执行一段时间
+	i := 0
+	for {
+		i++
+		fmt.Println("main goroutine: i = ", i)
+		time.Sleep(1 * time.Second)
+	}
 }
---
+++ GoRoutine/testGoroutine.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	//用go创建承载一个形参为空，返回值为空的函数的goroutine
+	go func() {
+		defer fmt.Println("A.defer")
+		//这是一个匿名函数，不是再开一个goroutine
+		func() {
+			defer fmt.Println("B.defer")
+			//退出当前的goroutine
+			runtime.Goexit()
+			fmt.Println("B")
+		}()
+		fmt.Println("A")
+	}()
+
+	//思考如何获取这个返回值，
+	//肯定不能写t:=下面这个，因为这个是个goroutine，它不支持阻塞操作的，它是异步操作（并行的）。 像a:= 5+10 其实是阻塞操作
+	//答案：使用管道机制：channel！！来通信
+	go func(a int, b int) bool {
+		fmt.Println("a = ", a, "b = ", b)
+		return true
+	}(10, 20)
+
+	//写个死循环防止main退出导致前面这个go func(){}()也退出了
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
