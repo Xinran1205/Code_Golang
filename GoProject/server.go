334fe0fdb133721d502c22f25d59a450c2786b09
Xinran1205 <id19037@bristol.ac.uk>
2023-06-24 16:27:54 +0800

    SocketProgramming notes

--- GoProject/server.go
+++ GoProject/server.go
@@ -23,7 +23,7 @@ func (s *Server) Handler(conn net.Conn) {
 
 // 启动服务器
 func (s *Server) Start() {
-	//socket listen, socket套接字可以理解为打包应用层和传输层的接口
+	//socket listen,创建监听器
 	listner, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
 	if err != nil {
 		fmt.Println("net.Listen err:", err)
@@ -33,11 +33,10 @@ func (s *Server) Start() {
 	defer listner.Close()
 
 	for {
-		//accept函数用于接受客户端的连接请求，并创建一个新的已连接套接字来处理客户端的请求。
 		//服务器在调用accept函数之后，会阻塞等待客户端的连接请求，一旦有客户端连接请求到达，
 		//accept函数会返回已连接套接字的描述符，服务器就可以使用该已连接套接字与客户端进行通信。
 
-		//服务器在调用accept函数时，会创建一个新的套接字用于该客户端连接的通信，并保留原始的套接字用于继续接受其它客户端的连接请求。
+		//服务器在调用accept函数时，会创建一个新的已连接套接字来处理客户端的请求。并保留原始的套接字用于继续接受其它客户端的连接请求。
 		//这样就可以同时处理多个客户端的连接请求，并与它们进行通信，而不会影响其它已经建立的连接。
 		conn, err := listner.Accept()
 
