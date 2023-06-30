38fa22b21b9693565d9a6147dd0cd2e7e81a68eb
Xinran1205 <id19037@bristol.ac.uk>
2023-06-30 17:31:36 +0800

    png

--- GoProject/user.go
+++ GoProject/user.go
@@ -19,13 +19,16 @@ func NewUser(conn net.Conn) *User {
 		conn: conn,
 	}
 
+	//在 Go 语言中，使用 go 关键字启动的 goroutine 是一个独立的执行线程，
+	//它 会在后台运行，不会阻塞当前的程序流程。当一个函数返回时，该函数中启动的 goroutine 会继续运行，直到该 goroutine 执行完毕或者程序退出。
+	//其实下面这句和创建用户没关系，他的作用是后面当我们user收到了消息，他就可以直接发给这个user对应的客户端
 	//启动监听当前user channel消息的goroutine
 	go user.ListenMessage()
 
 	return user
 }
 
-// 监听当前user channel的方法，一旦有消息，就直接发送给对端客户端
+// 监听当前user channel的方法，一旦有消息，就直接发送给对端客户端，注意是客户端，因为这个类还是属于服务器的部分
 func (u *User) ListenMessage() {
 	for {
 		msg := <-u.C
